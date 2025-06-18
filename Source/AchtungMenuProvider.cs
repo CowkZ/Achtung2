5ee6153e761ebdc045e3a3cf6e7423b76e5eddba
Cowk <63467150+CowkZ@users.noreply.github.com>
2025-06-18 15:15:08 -0300

    Update AchtungMenuProvider.cs

--- Source/AchtungMenuProvider.cs
+++ Source/AchtungMenuProvider.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
 
-namespace Achtung
+namespace AchtungMod
 {
-    internal class AchtungMenuProvider
+    public class AchtungMenuProvider : FloatMenuOptionProvider
     {
+        // Indica se o provider deve aparecer para pawns Drafted
+        public override bool Drafted => true;
+
+        // Indica se o provider deve aparecer para pawns Undrafted
+        public override bool Undrafted => true;
+
+        // Indica se suporta seleção múltipla
+        public override bool Multiselect => true;
+
+        // Decide quando deve fornecer opções (exemplo: só para colonistas humanos)
+        public override bool ShouldProvideOptions(Pawn pawn, LocalTargetInfo target, bool drafted)
+        {
+            return pawn != null && pawn.IsColonistPlayerControlled;
+        }
+
+        // Define as opções que o Float Menu vai exibir
+        public override IEnumerable<FloatMenuOption> GetOptions(Pawn pawn, LocalTargetInfo target, bool drafted)
+        {
+            yield return new FloatMenuOption(
+                "Achtung Custom Action!",
+                () =>
+                {
+                    Messages.Message($"Achtung: Opção clicada no pawn {pawn.LabelShort}.", MessageTypeDefOf.NeutralEvent);
+                }
+            );
+        }
     }
 }
